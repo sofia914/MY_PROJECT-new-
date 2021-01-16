daa32bf5d03f383d5f0a1089caf5b9961a4d3e8b
sofia914 <sofia914@users.noreply.github.com>
2021-01-16 15:57:33 +0900

    update

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 ## URL 별로 함수명이 같거나,
 ## route('/') 등의 주소가 같으면 안됩니다.
 client = MongoClient('localhost', 27017)
-db = client.seoul_art_gallery
+db = client.seoul_artgallery
 
 @app.route('/')
 def home():
     return render_template('map_page.html')
 
-@app.route('/art_gallery', methods=["GET"])
-def get_art_gallery():
+@app.route('/artgallery', methods=["GET"])
+def get_artgallery():
     # gu_receive 라는 변수에 전달받은 구 이름을 저장합니다.
     gu_receive = request.args.get('gu_give')
     # 구 이름에 해당하는 모든 미술관 목록을 불러옵니다.
-    art_gallery_list = list(db.art_gallery.find({'gu': gu_receive}, {'_id': False}))
-    # art_gallery_list 라는 키 값에 미술관 목록을 담아 클라이언트에게 반환합니다.
-    return jsonify({'result': 'success', 'art_gallery_list': art_gallery_list})
+    artgallery_list = list(db.artgallery.find({'gu': gu_receive}, {'_id': False}))
+    # artgallery_list 라는 키 값에 미술관 목록을 담아 클라이언트에게 반환합니다.
+    return jsonify({'result': 'success', 'artgallery_list': artgallery_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
--- crawl/artgallery_to_db.py
+++ crawl/artgallery_to_db.py
@@ -1,12 +1,11 @@
 import requests
 import pprint
 import time
-import urllib.parse
 from pymongo import MongoClient
 
-# 미술관 데이터는 seoul_art_gallery 이라는 데이터베이스에 저장하겠습니다.
+# 미술관 데이터는 seoul_artgallery 이라는 데이터베이스에 저장하겠습니다.
 client = MongoClient('localhost', 27017)
-db = client.seoul_art_gallery
+db = client.seoul_artgallery
 
 # 서울시 구마다 전시을 검색해보겠습니다.
 seoul_gu = ["종로구", "중구", "용산구", "성동구", "광진구", "동대문구", "중랑구", "성북구", "강북구", "도봉구", "노원구", "은평구", "서대문구", "마포구", "양천구", "강서구", "구로구", "금천구", "영등포구", "동작구", "관악구", "서초구", "강남구", "송파구", "강동구"]
@@ -35,18 +34,18 @@ for gu in seoul_gu:
     # '강님구 미술관', '종로구 미술관', '용산구 미술관' .. 을 반복해서 인코딩.
     keyword = f'{gu} 미술관'
     # 미술관 리스트를 받아옵니다.
-    art_gallery_list = get_naver_result(keyword)
+    artgallery_list = get_naver_result(keyword)
 
     # 구별 미술관 구분선입니다.
     print("*"*80 + gu)
 
-    for art_gallery in art_gallery_list:
+    for artgallery in artgallery_list:
         # 구 정보를 추가합니다.
-        art_gallery['gu'] = gu
+        artgallery['gu'] = gu
         # 미술관을 인쇄합니다.
-        pprint.pprint(art_gallery)
+        pprint.pprint(artgallery)
         # docs에 미술관을 추가합니다.
-        docs.append(art_gallery)
+        docs.append(artgallery)
 
 # 미술관 정보를 저장합니다.
-db.art_gallery.insert_many(docs)
+db.artgallery.insert_many(docs)
